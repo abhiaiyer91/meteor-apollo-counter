23d76f7ec2e348604f9332a89afdc0a994e08f0a
abhiaiyer91 <abhiaiyer91@gmail.com>
2016-06-27 22:57:11 -0700

    thunk examples

--- client/main.js
+++ client/main.js
@@ -1,15 +1,31 @@
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
 import React from 'react';
 import ApolloClient from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
+import ReduxThunk from 'redux-thunk';
 
 import AppWithData from '/imports/client/App';
 
-const client = new ApolloClient();
+const client = new ApolloClient({
+  reduxRootKey: 'apolloReducer',
+});
+
+function count(state = 0, action) {
+  if (action.type === "UPDATE_COUNT") {
+    return action.data;
+  }
+  return state;
+}
+
+const store = createStore(combineReducers({
+  count,
+  apolloReducer: client.reducer()
+}), {}, applyMiddleware(client.middleware(), ReduxThunk.withExtraArgument(client)));
 
 Meteor.startup(() => {
-  render(<ApolloProvider client={client}>
+  render(<ApolloProvider store={store} client={client}>
     <AppWithData />
   </ApolloProvider>, document.getElementById('app'));
 });
